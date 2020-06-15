d581ec4c06eb03f123cdf1ecd1870120526430b9
Lucas-Koolen <lucaskoolen04@gmail.com>
2020-06-15 08:51:26 +0200

    init.
    init.

--- commands/high.js
+++ commands/high.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) => {
 
     try{
 
-        message.channel.send("Vigo's mental state:", {files: ["./afbeeldingen/vigo-high.png"]})
+        message.channel.send("Vigo's mental state:", {files: ["./afbeeldingen/vigo-high-upgrade.png"]})
 
     } catch (error) {
         message.channel.send("Heb een error. probeer later opnieuw.");
